76c8ccfa3acc7fcfa0cf8d9b7822b958230b233b
sakxatadk <sakshyatadk04@gmail.com>
2025-12-02 18:14:09 +0530

    Guessinggame

---
+++ guessingnumb.py
@@ -0,0 +1,22 @@
+import random
+
+a = random.randint(0,100)
+i = 0
+
+while True:
+    b = int(input("Enter the number within 0 to 100:"))
+    i += 1
+    if b == a:
+        print("Congratulations! You guessed the correct number.")
+        break
+    elif b < a:
+        print("Your guess is too low. Try again!")
+    elif b > a:
+        print("Your guess is too high. Try again!")
+    else:
+        print("Invalid input.")
+    
+
+print("The computer number was:",a)
+print(f"Total attempts is {i}")
+
